404025f4b20144172c9027a3ab0b5d63cf545d79
Caio <caiocpe@gmail.com>
2022-01-04 16:52:01 -0300

    04/01/2022 16:51

--- react/chat-context/src/services/index.js
+++ react/chat-context/src/services/index.js
@@ -13,41 +13,53 @@ const url = "http://127.0.0.1:8000/";
 //     });
 // }
 
-export const sendUser = (nickname, password) => {
+export const connectSocket = () => {
+  const date = new Date();
+  const socket_id = `${date.getFullYear()}${
+    date.getMonth() + 1
+  }${date.getDate()}${date.getHours()}${date.getMinutes}${date.getSeconds()}${
+    date.getMilliseconds
+  }`;
 
-    console.log("stringfy", JSON.stringify({nickname, password}));
-    return fetch(url + "users/", {
-        method: 'POST',
-        headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({nickname, password})
-    });
-}
+  console.log(socket_id);
+
+  const ws = new WebSocket("ws://localhost:8000/ws/" + socket_id);
+
+  return ws;
+};
+
+export const sendUser = (nickname, password) => {
+  console.log("stringfy", JSON.stringify({ nickname, password }));
+  return fetch(url + "users/", {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ nickname, password }),
+  });
+};
 
 export const sendMessage = (userID, content) => {
-
-    console.log("stringfy", JSON.stringify({"owner_id": userID, content}));
-    return fetch(url + "messages/users/", {
-        method: 'POST',
-        headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({"owner_id": userID, content})
-    });
-}
+  console.log("stringfy", JSON.stringify({ owner_id: userID, content }));
+  return fetch(url + "messages/users/", {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ owner_id: userID, content }),
+  });
+};
 
 export const getMessages = () => {
-  
-    return fetch(url + "messages");
-}
+  return fetch(url + "messages");
+};
 
 export const getUserByNick = (nick) => {
-    return fetch(url + "users/?user_nick=" + nick);
-}
+  return fetch(url + "users/?user_nick=" + nick);
+};
 
 export const getUserById = (id) => {
-    return fetch(url + "users/?user_id=" + id);
-}
+  return fetch(url + "users/?user_id=" + id);
+};
