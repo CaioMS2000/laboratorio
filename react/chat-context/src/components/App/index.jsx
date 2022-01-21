b03d33e8f418f4e3beffb1494bab754667a6da6d
Caio <caiocpe@gmail.com>
2022-01-21 14:58:47 -0300

    20/01/2022 17:00

--- react/chat-context/src/components/App/index.jsx
+++ react/chat-context/src/components/App/index.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Helmet } from "react-helmet";
 
 import { GlobalStyles } from "../styles/Global";
 import * as S from "./styles";
@@ -10,6 +11,7 @@ import Home from "../Home";
 import SignUp from "../SignUp";
 import { useLogin } from "../context/Login";
 import { useSignUp } from "../context/SignUp";
+import { Card } from "../Card";
 
 function App() {
   const { logedIn, setLogedIn, login, user, setUser } = useLogin();
@@ -34,8 +36,17 @@ function App() {
 
   return (
     <>
-      <S.AppWrapper className="AppWrapper">
-        <GlobalStyles className="GlobalStyles" />
+      {/* <Helmet>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Oswald:wght@200;300;400;500;600;700&family=Permanent+Marker&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,600;0,800;0,900;1,100;1,200;1,300;1,400;1,600;1,800;1,900&display=swap"
+          rel="stylesheet"
+        />
+      </Helmet> */}
+      <GlobalStyles className="GlobalStyles" />
+      <Card inputs={["Name", "Age"]}></Card>
+      {/* <S.AppWrapper className="AppWrapper">
         <BrowserRouter>
           <S._Link
             to="/"
@@ -62,7 +73,7 @@ function App() {
             <Route path="*" element={<>{"THIS PAGE DOESN'T EXIST"}</>} />
           </Routes>
         </BrowserRouter>
-      </S.AppWrapper>
+      </S.AppWrapper> */}
     </>
   );
 }
