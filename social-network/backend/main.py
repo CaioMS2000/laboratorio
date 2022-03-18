ecf51f41dd1e08e47ebe01c7ef0608a3e690f9f5
Caio <caiocpe@gmail.com>
2022-03-18 16:55:59 -0300

    18/03/2022 16:55

--- social-network/backend/main.py
+++ social-network/backend/main.py
@@ -9,4 +9,8 @@ app = fastapi.FastAPI()
 
 @app.post("/api/token")
 async def generate_token(form_data: fastapi.security.OAuth2PasswordRequestForm = fastapi.Depends(), db: sqlalchemy.orm.Session = fastapi.Depends(services.get_database)):
-    pass
+    user = dict(username=form_data.username, password=form_data.password)
+    valid = True
+
+    if not valid:
+        raise fastapi.HTTPException(status_code=401, detail="Invalid Credentials")
--- social-network/backend/services.py
+++ social-network/backend/services.py
@@ -1,5 +1,8 @@
 import database
 import sqlalchemy.orm
+import jwt
+
+JWT_SECRET = "secret code"
 
 def create_database():
     return database.Base.metadata.create_all(bind=database.engine)
@@ -14,3 +17,8 @@ def get_database():
 
 async def authenticate_user(db: sqlalchemy.orm.Session):
     pass
+
+async def create_token():
+    token = jwt.encode({}, JWT_SECRET)
+
+    return dict(access_token=token, token_type="bearer")
