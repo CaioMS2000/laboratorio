915b80564f86a262b8fa46a6516aa47614f63ec0
Caio <caiocpe@gmail.com>
2022-04-26 16:54:39 -0300

    iniciando com Django

--- react/chat-context/src/components/App/index.jsx
+++ react/chat-context/src/components/App/index.jsx
@@ -18,32 +18,8 @@ function App() {
   const { signedUp, setSignedUp, signingUp, setSigningUp } = useSignUp();
   const nickname = user.nickname;
 
-  // useEffect(() => {
-  //   async function main() {
-  //     const res = await API.getToken("caio", "123");
-  //     console.log(res);
-
-  //     if (res.ok) {
-  //       const data = await res.json();
-  //       console.log(data);
-  //     } else {
-  //       console.log("erro ao buscar o token");
-  //     }
-  //   }
-
-  //   main();
-  // }, []);
-
   return (
     <>
-      {/* <Helmet>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Oswald:wght@200;300;400;500;600;700&family=Permanent+Marker&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,600;0,800;0,900;1,100;1,200;1,300;1,400;1,600;1,800;1,900&display=swap"
-          rel="stylesheet"
-        />
-      </Helmet> */}
       <GlobalStyles className="GlobalStyles" />
       {/* <Card inputs={["Name", "Age"]}></Card> */}
       <S.AppWrapper className="AppWrapper">
