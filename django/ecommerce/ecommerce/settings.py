77639b73b201d4d21510ad232e61660c7fc926a0
Caio <caiocpe@gmail.com>
2022-05-11 16:42:52 -0300

    continuando com Django. Projeto de e-commerce (57 min caso necessário)

--- django/ecommerce/ecommerce/settings.py
+++ django/ecommerce/ecommerce/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import environ
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -51,6 +52,7 @@ INSTALLED_APPS = [
     'products.apps.ProductsConfig',
     'cart.apps.CartConfig',
     'orders.apps.OrdersConfig',
+    'payments.apps.PaymentsConfig'
 ]
 
 MIDDLEWARE = [
@@ -160,6 +162,12 @@ CART_ITEM_MAX_QUANTITY = 20
 
 # Crispy
 CRYSPY_TEMPLATE_PACK = "bootstrap4"
+
+# Mercado Pago
+env = environ.Env()
+env.read_env(str(BASE_DIR/'.env'))
+MERCADO_PAGO_PUBLIC_KEY = env("MERCADO_PAGO_PUBLIC_KEY")
+MERCADO_PAGO_ACCESS_TOKEN = env("MERCADO_PAGO_ACCESS_TOKEN")
 
 # when project is in a Dcoker container
 """
