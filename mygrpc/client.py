ec7d80ebcfb16346fff8c26614a91aebfe8fe0c9
Caio <caiocpe@gmail.com>
2022-12-10 14:37:53 -0300

    trabalho grpc pronto for real

---
+++ mygrpc/client.py
@@ -0,0 +1,28 @@
+from os import system
+system('clear')
+
+from concurrent import futures
+import time
+from random import randint
+import grpc
+from grpc import _server
+import sum_pb2
+import sum_pb2_grpc
+
+PORT = 50051
+HOST = "localhost"
+min = 0
+max = 100
+
+def run():
+    with grpc.insecure_channel(f"{HOST}:{PORT}") as channel:
+        stub = sum_pb2_grpc.SumNumbersStub(channel)
+        a = randint(min, max)
+        b = randint(min, max)
+
+        add_request = sum_pb2.AddRequest(a= a, b= b)
+        add_reply = stub.Add(add_request)
+        print(f"{a} + {b} = {add_reply}")
+
+if __name__ == "__main__":
+    run()
---
+++ mygrpc/server.py
@@ -0,0 +1,34 @@
+from os import system
+system('clear')
+
+from concurrent import futures
+import time
+import grpc
+from grpc import _server
+import sum_pb2
+import sum_pb2_grpc
+
+PORT = 50051
+HOST = "localhost"
+
+class Servicer(sum_pb2_grpc.SumNumbersServicer):
+    def Add(self, request, context):
+        print(request)
+
+        add_reply = sum_pb2.AddReply()
+        add_reply.r = request.a + request.b
+        
+        # return super().Add(request, context)
+        return add_reply
+
+def serve():
+    server:_server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+    sum_pb2_grpc.add_SumNumbersServicer_to_server(Servicer(), server)
+    # serve._add_insecure_port(f"localhost:{PORT}")
+    server.add_insecure_port(f"{HOST}:{PORT}")
+    server.start()
+    print(f"Listenning on port {PORT}")
+    server.wait_for_termination()
+
+if __name__ == "__main__":
+    serve()
