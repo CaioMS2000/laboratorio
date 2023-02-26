2f62356995e4c9963b8d2ca0fe4760a448111e9d
Caio <caiocpe@gmail.com>
2023-02-26 17:32:43 -0300

    login pronto, porem quebrado

--- react/react-router/src/index.js
+++ react/react-router/src/index.js
@@ -7,13 +7,21 @@ import {
 
 import './index.css';
 import App from './App';
-import Root from "./routes/root";
 
-const router = createBrowserRouter([{
-  path: "/",
-  element: <App />,
-  // element: <Root />,
-}]);
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/1",
+    element: <div>page n1</div>,
+  },
+  {
+    path: "/2",
+    element: <div>page n2</div>,
+  },
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
