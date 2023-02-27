2a673dce1cb0e105b807a3663f1c11151067d852
Caio <caiocpe@gmail.com>
2023-02-26 23:07:03 -0300

    consertando o login

--- html/bootstrap/login/index.js
+++ html/bootstrap/login/index.js
@@ -1,17 +1,9 @@
-const img = document.querySelector('img')
+const DEBUGING = false;
 
 window.addEventListener("resize", (event) => {
-    // console.log(window.innerWidth)
-    // console.log(document.querySelector('#data-side').offsetWidth)
-    // console.log(document.querySelector('#data-side').style.padding)
 
-    // console.log(window.getComputedStyle(document.querySelector('#data-side')))
-    console.log(`${window.getComputedStyle(document.querySelector('#data-side'))['padding-left'].replace('px', '')} - ${window.getComputedStyle(document.querySelector('#data-side'))['padding-right'].replace('px', '')}`)
+    if(DEBUGING){
+        document.querySelector('input').value = window.innerWidth
+    }
 
-    if(window.innerWidth > 1000){
-        img.src = 'img_1000.jpg'
-    }
-    else{
-        img.src = 'img.jpg'
-    }
 });
